f6444b06f49bd62f1aa05adac14ae78c35bd6f78
Robert Lopez <robertlopez@Roberts-MBP.lan>
2022-03-23 10:59:09 -0700

    finished user CRUD

---
+++ api/serializers/user.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        # https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#django.contrib.auth.get_user_model
+        model = get_user_model()
+        fields = ('id', 'email', 'password')
+        extra_kwargs = { 'password': { 'write_only': True, 'min_length': 5 } }
+
+    # This create method will be used for model creation
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
