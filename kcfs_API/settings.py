380dfa589481effd91a2ad8ffb602090303d10ba
Robert Lopez <robertlopez@Roberts-MBP.lan>
2022-03-23 10:35:59 -0700

    Update project settings to include psql and api

--- kcfs_API/settings.py
+++ kcfs_API/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Our Apps
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,8 @@ WSGI_APPLICATION = 'kcfs_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kcfs_API',
     }
 }
 
